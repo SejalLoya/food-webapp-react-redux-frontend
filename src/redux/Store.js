f7a3c14516e50a30c0e8b12bf3eaf849cee15c29
Sejal Loya <sejalloya9@gmail.com>
2025-03-23 20:52:52 +0530

    currently removing backend code

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import CartSlice from "./slices/CartSlice";
 import CategorySlice from "./slices/CategorySlice";
 import SearchSlice from "./slices/SearchSlice";
-import AuthSlice from "./slices/AuthSlice";
 
 const Store = configureStore({
   reducer: {
     cart: CartSlice,
     category: CategorySlice,
     search: SearchSlice,
-    auth: AuthSlice,
   },
 });
 
