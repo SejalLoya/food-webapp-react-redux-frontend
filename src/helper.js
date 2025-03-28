83ec598aea2bc27bed3de7461d8c7eb8513f2dec
Sejal Loya <sejalloya9@gmail.com>
2025-03-28 23:22:57 +0530

    added axios imports

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const getCart = async(user) => {
     const res = await axios.get(`https://swiggato-nodejs-backend.onrender.com/api/get-cart/${user._id}`);
     const data = await res.data;
