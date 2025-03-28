d83f4ceaa757fd9ab2fdccc3971ffeee9e2a4c69
Sejal Loya <sejalloya9@gmail.com>
2025-03-28 23:19:50 +0530

    avoiding undefined errors

--- src/helper.js
+++ src/helper.js
@@ -1,16 +1,5 @@
-export const getCart = async (user) => {
-    if (!user || !user._id) {
-        return [];
-    }
-
-    try {
-        const res = await axios.get(`https://swiggato-nodejs-backend.onrender.com/api/get-cart/${user._id}`, {
-            withCredentials: true,
-        });
-
-        return res.data;
-    } catch (error) {
-        console.error("Error fetching cart:", error.message);
-        return [];
-    }
-};
+export const getCart = async(user) => {
+    const res = await axios.get(`https://swiggato-nodejs-backend.onrender.com/api/get-cart/${user._id}`);
+    const data = await res.data;
+    return data;
+}
